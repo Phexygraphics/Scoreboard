b2fc58a0f26d347d6aa942bb47ea2a715044ed6e
Phexygraphics <Phexygraphics12@gmail.com>
2023-02-09 10:06:35 +0000

    commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let homePoints = 0;
+function increaseOne(){
+    homePoints+= 1;
+    homeIncrease.innerHTML = homePoints;
+}
+function increaseTwo(){
+    homePoints+= 2;
+    homeIncreaseTwo.innerHTML = homePoints;
+}
+function increaseThree(){
+    homePoints+= 3;
+    homeIncreaseThree.innerHTML = homePoints;
+}
+function awayIncreaseOne(){
+    homePoints+= 1;
+    awayIncrease.innerHTML = homePoints;
+}
+function awayIncreaseTwo(){
+    homePoints+= 2;
+    awayIncreasebyTwo.innerHTML = homePoints;
+}
+function awayIncreaseThree(){
+    homePoints+= 3;
+    awayIncreasebyThree.innerHTML = homePoints;
+}
